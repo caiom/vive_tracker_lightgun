39de6a23745d17a2eea20cca7ee178783348fa99
Caio Mendes <caioctmendes@gmail.com>
2024-12-10 00:41:22 -0300

    update esp32

--- esp32/Notify/Notify.ino
+++ esp32/Notify/Notify.ino
@@ -1,68 +1,60 @@
-/*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-    updated by chegewara
-
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 4fafc201-1fb5-459e-8fcc-c5c9c331914b
-   And has a characteristic of: beb5483e-36e1-4688-b7f5-ea07361b26a8
-
-   The design of creating the BLE server is:
-   1. Create a BLE Server
-   2. Create a BLE Service
-   3. Create a BLE Characteristic on the Service
-   4. Create a BLE Descriptor on the characteristic
-   5. Start the service.
-   6. Start advertising.
-
-   A connect handler associated with the server starts a background task that performs notification
-   every couple of seconds.
-*/
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include <BLE2901.h>
 
+// BLE objects and variables
 BLEServer *pServer = NULL;
 BLECharacteristic *pCharacteristic = NULL;
 BLE2901 *descriptor_2901 = NULL;
 
+// Connection tracking
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-uint32_t value = 0;
 
+// Solenoid and button states
 #define BTN_TRIGGER 10
 #define BTN_A 7
 #define BTN_B 21
-
 #define SOLENOID 2
 
 int lastButtonState = 0;
 
+// Trigger button press timing
+int triggerStartPress = 0;      // When the trigger was first pressed
+int triggerTimePressed = 0;     // How long the trigger has been held down
+int triggerLastState = HIGH;    // Last read state of the trigger button
+bool solenoidActive = false;    // Whether the solenoid functionality is active
 
-int triggerStartPress = 0;
-int triggerTimePressed = 0;
-int triggerTimeReleased = 0;
-int triggerTimeSinceRelease = 0;
-int triggerLastState = HIGH;
+// General button states
+int buttonState = 0;            // Composed bitfield of all button states
+int triggerState = 0;
+int buttonBState = 0;
+int buttonBStartPress = 0;
+int buttonBTimePressed = 0;
+int buttonBLastState = HIGH;
 
-// See the following for generating UUIDs:
+unsigned long lastNotification = 0;  // Last time we sent a BLE notification
+
+// BLE UUIDs
 // https://www.uuidgenerator.net/
-
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
+
+// Callback class to handle BLE server events
 class MyServerCallbacks : public BLEServerCallbacks {
   void onConnect(BLEServer *pServer, esp_ble_gatts_cb_param_t *param) {
     deviceConnected = true;
+
+    // Copy the remote device address
     esp_bd_addr_t remote_bda;
     memcpy(remote_bda, param->connect.remote_bda, sizeof(esp_bd_addr_t));
 
-    // Request a connection parameter update
+    // Request a connection parameter update to reduce latency/power consumption
     pServer->updateConnParams(remote_bda, 6, 6, 0, 100);
-  };
+  }
 
   void onDisconnect(BLEServer *pServer) {
     deviceConnected = false;
@@ -70,127 +62,156 @@ class MyServerCallbacks : public BLEServerCallbacks {
 };
 
 void setup() {
-
+  // Set pin modes
   pinMode(BTN_TRIGGER, INPUT_PULLUP);
   pinMode(BTN_A, INPUT_PULLUP);
   pinMode(BTN_B, INPUT_PULLUP);
-
   pinMode(SOLENOID, OUTPUT);
-  
+  digitalWrite(SOLENOID, LOW);
+
   Serial.begin(115200);
 
-  // Create the BLE Device
+  // Initialize the BLE Device with a given name
   BLEDevice::init("ESP32");
 
-  // Create the BLE Server
+  // Create the BLE Server and set its callbacks
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
   // Create the BLE Service
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  // Create a BLE Characteristic
+  // Create a BLE Characteristic with read, write, notify and indicate properties
   pCharacteristic = pService->createCharacteristic(
     CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE
+    BLECharacteristic::PROPERTY_READ |
+    BLECharacteristic::PROPERTY_WRITE |
+    BLECharacteristic::PROPERTY_NOTIFY |
+    BLECharacteristic::PROPERTY_INDICATE
   );
 
-  // Creates BLE Descriptor 0x2902: Client Characteristic Configuration Descriptor (CCCD)
+  // Add a Client Characteristic Configuration Descriptor (CCCD) to allow clients to enable/disable notifications
   pCharacteristic->addDescriptor(new BLE2902());
-  // Adds also the Characteristic User Description - 0x2901 descriptor
+
+  // Add a Characteristic User Description (0x2901) descriptor
   descriptor_2901 = new BLE2901();
   descriptor_2901->setDescription("My own description for this characteristic.");
-  descriptor_2901->setAccessPermissions(ESP_GATT_PERM_READ);  // enforce read only - default is Read|Write
+  descriptor_2901->setAccessPermissions(ESP_GATT_PERM_READ);  
   pCharacteristic->addDescriptor(descriptor_2901);
 
   // Start the service
   pService->start();
 
-  // Start advertising
+  // Start advertising the service
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(false);
-  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  pAdvertising->setMinPreferred(0x0);  // Not advertising this parameter
   BLEDevice::startAdvertising();
-  Serial.println("Waiting a client connection to notify...");
+
+  Serial.println("Waiting for a client to connect...");
 }
 
 void loop() {
-  // notify changed value
+  // Read current input states
+  triggerState = digitalRead(BTN_TRIGGER);
+  int buttonAState = digitalRead(BTN_A);
+  buttonBState = digitalRead(BTN_B); // <- Make sure the semicolon is here
 
-  int buttonState = 0;
-  int triggerState = digitalRead(BTN_TRIGGER);
-  buttonState |= triggerState;
-  buttonState |= digitalRead(BTN_A) << 2;
-  buttonState |= digitalRead(BTN_B) << 3;
+  // Compose buttonState:
+  // bit 0: triggerState
+  // bit 2: buttonAState
+  // bit 3: buttonBState
+  // The other bits are 0 for now.
+  buttonState = 0;
+  buttonState |= (triggerState & 0x01);        // puts triggerState in bit 0
+  buttonState |= (buttonAState & 0x01) << 2;   // puts buttonAState in bit 2
+  buttonState |= (buttonBState & 0x01) << 3;   // puts buttonBState in bit 3
 
-  if (triggerState == LOW && triggerLastState == HIGH)
-     {
-      triggerStartPress = millis();
-      digitalWrite(SOLENOID, HIGH);
-      triggerLastState = LOW;
-      Serial.println("Pressed");
-      Serial.print(triggerStartPress);
-    }
-
-    if (triggerState == HIGH && triggerLastState == LOW) 
-    {
-    triggerLastState = HIGH;
-    triggerStartPress = 0;
-    triggerTimeReleased = millis();
-    Serial.println("Released");
-    digitalWrite(SOLENOID, LOW);
+  // Handle trigger button press/release logic and solenoid control
+  if (triggerState == LOW && triggerLastState == HIGH && solenoidActive) {
+    // Trigger just pressed
+    triggerStartPress = millis();
+    digitalWrite(SOLENOID, HIGH);
+    triggerLastState = LOW;
+    Serial.println("Trigger pressed");
   }
 
-  if (deviceConnected) 
-  {
-    if (buttonState != lastButtonState)
-    {
+  if (triggerState == HIGH && triggerLastState == LOW && solenoidActive) {
+    // Trigger just released
+    triggerLastState = HIGH;
+    triggerStartPress = 0;
+    digitalWrite(SOLENOID, LOW);
+    Serial.println("Trigger released");
+  }
+
+  // Handle long-press on button B to toggle solenoidActive
+  if (buttonBState == LOW && buttonBLastState == HIGH) {
+    // Button B just pressed
+    buttonBStartPress = millis();
+    buttonBLastState = LOW;
+  }
+
+  if (buttonBState == HIGH && buttonBLastState == LOW) {
+    // Button B just released
+    buttonBLastState = HIGH;
+    buttonBStartPress = 0;
+  }
+
+  // If Button B is held for more than 3 seconds, toggle solenoidActive
+  if (buttonBState == LOW && ((millis() - buttonBStartPress) > 3000)) {
+    buttonBStartPress = millis();
+    solenoidActive = !solenoidActive;
+  }
+
+  // If a client is connected, send notifications when state changes or periodically
+  if (deviceConnected) {
+    // Notify on any change in button state or every 200ms
+    if (buttonState != lastButtonState || ((millis() - lastNotification) > 200)) {
       lastButtonState = buttonState;
+      // Send the buttonState as a 4-byte value
       pCharacteristic->setValue((uint8_t *)&buttonState, 4);
       pCharacteristic->notify();
       Serial.println("Sending notification...");
+      lastNotification = millis();
     }
   }
 
-    
-
-
-
-  if (triggerState == LOW) {
+  // If trigger is held down and solenoid is active, handle timing-based solenoid pulses
+  if (triggerState == LOW && solenoidActive) {
     triggerTimePressed = millis() - triggerStartPress;
 
-    if (triggerTimePressed >= 25 && triggerTimePressed <= 300)
-    {
+    // If held for 25-300ms, keep solenoid off
+    if (triggerTimePressed >= 25 && triggerTimePressed <= 300) {
       digitalWrite(SOLENOID, LOW);
     }
 
-    if (triggerTimePressed > 300) 
-    {
-      int cycle_pos = triggerTimePressed % 125;
-      if (cycle_pos >= 50 && cycle_pos < 75)
-      {
+    // After 300ms, pulse the solenoid in a pattern
+    if (triggerTimePressed > 300) {
+      int cycle_pos = triggerTimePressed % 125; // Cycle every 125ms
+      if (cycle_pos >= 50 && cycle_pos < 75) {
         digitalWrite(SOLENOID, HIGH);
-      }
-      else
-      {
+      } else {
         digitalWrite(SOLENOID, LOW);
       }
     }
   }
 
-  // disconnecting
+  // Handle disconnect scenario
   if (!deviceConnected && oldDeviceConnected) {
-    delay(500);                   // give the bluetooth stack the chance to get things ready
-    pServer->startAdvertising();  // restart advertising
-    Serial.println("start advertising");
-    oldDeviceConnected = deviceConnected;
-  }
-  // connecting
-  if (deviceConnected && !oldDeviceConnected) {
-    // do stuff here on connecting
+    // Give the stack time and then start advertising again
+    delay(500);
+    pServer->startAdvertising();
+    Serial.println("Starting advertising again...");
     oldDeviceConnected = deviceConnected;
   }
 
+  // Handle connect scenario
+  if (deviceConnected && !oldDeviceConnected) {
+    // If needed, do something special upon connection
+    oldDeviceConnected = deviceConnected;
+  }
+
+  // A small delay to reduce busy-waiting
   delay(1);
 }
